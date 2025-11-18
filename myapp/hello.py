b73423a4cff59b15a9d3f6d0c215fc249fa371c4
SRUNNOOBIE <85465677+MENGSRUN@users.noreply.github.com>
2025-11-18 10:20:03 +0000

    modify hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,11 @@
-import fire
+import argparse
 
 def hello(name="World"):
-  return "Hello %s!" % name
+    return f"Hello {name}!"
 
-if __name__ == '__main__':
-  fire.Fire(hello)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument("--name", type=str, default="World", help="Name to greet")
+    args = parser.parse_args()
+
+    print(hello(args.name))
